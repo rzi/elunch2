dfb6da18549380d92e0031a705073ea7c73a3bf2
rzi <rzi@vp.pl>
2019-02-08 21:52:18 +0100

    08/02

--- www/tabela2.php
+++ www/tabela2.php
@@ -7,17 +7,17 @@ connection();
 echo "<table id='table2' class='fixed_headers_d' style='width:100%;' border='1'>
 <thead style='width:98%;'>
 	<tr style='width:98%;'>
-		<td style='width:250px;'>Imię</td>
-		<td style='width:250px;'>Nazwisko</td>
-		<td style='width:250px;'>Dostawca</td>
-		<td > Danie</td> 
+		<th style='width:250px;'>Imię</th>
+		<th style='width:250px;'>Nazwisko</th>
+		<th style='width:250px;'>Dostawca</th>
+		<th style='width:250px;'> Danie</th>
 	</tr>
 </thead>";
 echo "<tbody style='width:100%;'>";
 if($result = mysqli_query($link,"select * from login, orders WHERE data='$date1'  and login.id_users = orders.id_users order by dostawca ASC")){
 
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr style='width:98%;'><td style='width:250px;'>";
+        echo "<tr style='width:100%;'><td style='width:250px;'>";
         echo $row["my_first_name"];
         echo "</td>";
         echo "<td style='width:250px;'>";
@@ -26,7 +26,7 @@ if($result = mysqli_query($link,"select * from login, orders WHERE data='$date1'
         echo "<td style='width:250px'>";
         echo $row["dostawca"];
         echo "</td>";
-        echo "<td>";
+        echo "<td style='width:450px'>";
         echo $row["numer"];
         echo "</td>";
         //echo "<tr>";
